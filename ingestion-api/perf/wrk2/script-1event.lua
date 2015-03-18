a2b95684288dc9c3bc0d241da9c0c2a125217dc6
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-03-18 13:11:01 +0000

    new perf test profile (1 batch of 100 events)

--- ingestion-api/perf/wrk2/script-1event.lua
+++ ingestion-api/perf/wrk2/script-1event.lua
@@ -3,4 +3,14 @@
 
 wrk.method = "POST"
 wrk.headers["Content-Type"] = "application/json"
-wrk.body   = "[ { \"timestamp\": 1424444803463, \"sourceId\": \"3aw4sedrtcyvgbuhjkn\",  \"eventName\": \"user.clicked\", \"page\": \"orders\", \"item\": \"sku-1234\" }]"
+wrk.body   = [[
+[
+  {
+    "timestamp": 1424444803463,
+    "sourceId": "3aw4sedrtcyvgbuhjkn",
+    "eventName": "user.clicked",
+    "page": "orders",
+    "item": "sku-1234"
+  }
+]
+]]
