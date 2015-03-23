bb38c0ca5f592466d0000910b4e849344810a674
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-03-23 22:23:19 +0000

    fix Kafka' stop script

--- deployments/kafka/stop.sh
+++ deployments/kafka/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # add services for client, peer, and election connections
-kubectl.sh delete -f $(dirname $0)/kafka-client-service.yaml
+kubectl.sh delete -f $(dirname $0)/kafka-service.yaml
 
 # now delete controllers
 seq 3 | xargs -I {} kubectl.sh resize rc kafka-{} --replicas=0
