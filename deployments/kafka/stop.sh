5c6cd14b814abd3994987c0eda517db49fe29be4
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-03-23 20:23:01 +0000

    small fix

--- deployments/kafka/stop.sh
+++ deployments/kafka/stop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # add services for client, peer, and election connections
-kubectl.sh delete -f $(dirname $0)/zookeeper-client-service.yaml
+kubectl.sh delete -f $(dirname $0)/kafka-client-service.yaml
 
 # now delete controllers
-seq 3 | xargs -I {} kubectl.sh resize rc zookeeper-{} --replicas=0
-seq 3 | xargs -I {} kubectl.sh delete -f $(dirname $0)/zookeeper-ctrl-{}.yaml
+seq 3 | xargs -I {} kubectl.sh resize rc kafka-{} --replicas=0
+seq 3 | xargs -I {} kubectl.sh delete -f $(dirname $0)/kafka-ctrl-{}.yaml
