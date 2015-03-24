04780b581778c1bb6517184d8adae14aa27c4b22
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-03-24 10:44:16 +0000

    small sequence fix

--- deployments/ingestion-api/start.sh
+++ deployments/ingestion-api/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# add services connections
-kubectl.sh create -f $(dirname $0)/ingestion-api-service.yaml
 
 # now add controller
 kubectl.sh create -f $(dirname $0)/ingestion-api-ctrl.yaml
+
+sleep 5
+
+# add services connections
+kubectl.sh create -f $(dirname $0)/ingestion-api-service.yaml
