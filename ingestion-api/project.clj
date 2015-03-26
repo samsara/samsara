a9f037ef65dd3a18dac80d6ceb0b53df0034bb15
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-03-26 12:50:41 +0000

    added lein-bin as project's plugin

--- ingestion-api/project.clj
+++ ingestion-api/project.clj
@@ -1,8 +1,8 @@
-(defproject ingestion-api "0.1.0"
+(defproject ingestion-api "0.2.0-SNAPSHOT"
   :description "Ingestion APIs for Samsara's analytics"
   :url "https://samsara.github.com/"
-  :license {:name "Apache 2 License"
-            :url "http://www.apache.org/licenses/"}
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.19"]
                  [ring/ring-devel "1.3.2"]
@@ -17,7 +17,11 @@
                  [org.clojure/tools.cli "0.3.1"]]
 
   :main ingestion-api.core
-  :jvm-opts ["-server" "-Dfile.encoding=utf-8"]
-  :profiles {:uberjar {:aot :all}}
 
-  :bin {:name "ingestion-api" :bootclasspathX true})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]
+                             [lein-bin "0.3.5"]]}}
+
+  :jvm-opts ["-server" "-Dfile.encoding=utf-8"]
+  :bin {:name "ingestion-api" :bootclasspath false})
