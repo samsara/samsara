e3395aa991fdbaead83ea73b57cb9d722a81238f
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-04-01 11:15:29 +0100

    adding Samsara's elasticsearch image

---
+++ docker-images/els/configure-and-start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# REQUIRED:
+#   ZOOKEEPER_PORT_2181_TCP_ADDR
+
+export CONFIG_FILE=/etc/elasticsearch/elasticsearch.yml
+# replace variables in template with environment values
+echo "TEMPLATE: generating configuation."
+perl -pe 's/%%([A-Za-z0-9_]+)%%/defined $ENV{$1} ? $ENV{$1} : $&/eg' < ${CONFIG_FILE}.tmpl > $CONFIG_FILE
+
+# check if all properties have been replaced
+if grep -qoP '%%[^%]+%%' $CONFIG_FILE ; then
+    echo "ERROR: Not all variable have been resolved,"
+    echo "       please set the following variables in your environment:"
+    grep -oP '%%[^%]+%%' $CONFIG_FILE | sed 's/%//g' | sort -u
+    exit 1
+fi
+
+
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
