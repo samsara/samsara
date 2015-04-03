b431110c7e54b29cc35741470a83dd8db016cacc
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-04-03 17:51:44 +0100

    fix advertised IP problem with kafka

--- docker-images/kafka/configure-and-start.sh
+++ docker-images/kafka/configure-and-start.sh
@@ -4,19 +4,21 @@
 #   KAFKA_BROKER_ID
 #   ZOOKEEPER_PORT_2181_TCP_ADDR
 export HOSTNAME=${HOSTNAME-kafka$KAFKA_BROKER_ID}
+export IP=`ip ro get 8.8.8.8 | grep -oP "(?<=src )(\S+)"`
 export KAFKA_ENABLE_REPORTING_STATSD=${KAFKA_ENABLE_REPORTING_STATSD-false}
 export STATSD_PORT_8125_TCP_ADDR=${STATSD_PORT_8125_TCP_ADDR}
 
 
+export CONFIG_FILE=/opt/kafka/config/server.properties
 # replace variables in template with environment values
 echo "TEMPLATE: generating configuation."
-perl -pe 's/%%([A-Za-z0-9_]+)%%/defined $ENV{$1} ? $ENV{$1} : $&/eg' < /opt/kafka/config/server.properties.tmpl > /opt/kafka/config/server.properties
+perl -pe 's/%%([A-Za-z0-9_]+)%%/defined $ENV{$1} ? $ENV{$1} : $&/eg' < ${CONFIG_FILE}.tmpl > $CONFIG_FILE
 
 # check if all properties have been replaced
-if grep -qoP '%%[^%]+%%' /opt/kafka/config/server.properties ; then
+if grep -qoP '%%[^%]+%%' $CONFIG_FILE ; then
     echo "ERROR: Not all variable have been resolved,"
     echo "       please set the following variables in your environment:"
-    grep -oP '%%[^%]+%%' /opt/kafka/config/server.properties | sed 's/%//g' | sort -u
+    grep -oP '%%[^%]+%%' $CONFIG_FILE | sed 's/%//g' | sort -u
     exit 1
 fi
 
