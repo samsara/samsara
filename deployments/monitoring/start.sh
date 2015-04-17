8321f8da900501e9c42e55a54261bb378e6fa109
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-04-17 11:15:48 +0100

    fixed monitoring start script

--- deployments/monitoring/start.sh
+++ deployments/monitoring/start.sh
@@ -2,10 +2,7 @@
 
 
 # now add controller
-kubectl.sh create -f $(dirname $0)/els-ctrl.yaml
-
-# now add controller
-kubectl.sh create -f $(dirname $0)/kibana-ctrl.yaml
+kubectl.sh create -f $(dirname $0)/monitoring-ctrl.yaml
 
 # now add services
 kubectl.sh create -f $(dirname $0)/riemann-service.yaml
