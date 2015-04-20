581b828ab5258f0d8356dd54b681c1e375abe557
DayoOliyide <dayo.oliyide@gmail.com>
2015-04-20 21:28:34 +0100

    Added Circle CI config

--- ingestion-api/test/intestion_api/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns intestion-api.core-test
-  (:require [clojure.test :refer :all]
-            [intestion-api.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
