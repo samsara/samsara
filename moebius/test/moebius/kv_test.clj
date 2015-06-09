9f17f20a79e90cb60099b6793485120c2d52028e
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-06-09 22:27:15 +0100

    refactor tests

--- moebius/test/moebius/kv_test.clj
+++ moebius/test/moebius/kv_test.clj
@@ -7,64 +7,57 @@
 (def kvstore (kv/make-in-memory-kvstore))
 
 
-(facts "about KV protocols"
 
-       (fact "I should be able to set a property with any value"
+(facts "about KV protocols: I should be able to set a property with any value"
 
-             ;; assertion                             expected result
-             (-> kvstore
-                 (kv/set "s1" "key1" "v1")
-                 (kv/get "s1" "key1"))         =>       "v1"
+       ;; assertion                             expected result
+       (-> kvstore
+           (kv/set "s1" "key1" "v1")
+           (kv/get "s1" "key1"))         =>       "v1"
 
-             (-> kvstore
-                 (kv/set "s1" "key1" 1)
-                 (kv/get "s1" "key1"))         =>       1
+       (-> kvstore
+           (kv/set "s1" "key1" 1)
+           (kv/get "s1" "key1"))         =>       1
 
-             (-> kvstore
-                 (kv/set "s1" "key1" true)
-                 (kv/get "s1" "key1"))         =>       true
+       (-> kvstore
+           (kv/set "s1" "key1" true)
+           (kv/get "s1" "key1"))         =>       true
 
-             (-> kvstore
-                 (kv/set "s1" "key1" :a-value)
-                 (kv/get "s1" "key1"))         =>       :a-value
-             )
-
-
-       (fact "properties are independent across sourceId"
-
-             ;; assertion                             expected result
-             (-> kvstore
-                 (kv/set "s1" "key1" "v1")
-                 (kv/set "s2" "key1" "v2")
-                 (kv/get "s1" "key1"))         =>       "v1"
-
-             (-> kvstore
-                 (kv/set "s1" "key1" "v1")
-                 (kv/set "s2" "key1" "v2")
-                 (kv/get "s2" "key1"))         =>       "v2"
-
-                 )
-
-
-       (fact "if a key hasn't been set or remove should return `nil`"
-
-             ;; assertion                             expected result
-             (-> kvstore
-                 (kv/set "s1" "key1" "v1")
-                 (kv/set "s2" "key1" "v2")
-                 (kv/get "s3" "key1"))         =>       nil
-
-             (-> kvstore
-                 (kv/set "s1" "key1" "v1")
-                 (kv/set "s2" "key1" "v2")
-                 (kv/get "s1" "key5"))         =>       nil
-
-             (-> kvstore
-                 (kv/set "s1" "key1" "v1")
-                 (kv/set "s1" "key1" nil)
-                 (kv/get "s1" "key1"))         =>       nil
-                 )
+       (-> kvstore
+           (kv/set "s1" "key1" :a-value)
+           (kv/get "s1" "key1"))         =>       :a-value)
 
 
 
-       )
+(facts "about KV protocols: properties are independent across sourceId"
+
+       ;; assertion                             expected result
+       (-> kvstore
+           (kv/set "s1" "key1" "v1")
+           (kv/set "s2" "key1" "v2")
+           (kv/get "s1" "key1"))         =>       "v1"
+
+       (-> kvstore
+           (kv/set "s1" "key1" "v1")
+           (kv/set "s2" "key1" "v2")
+           (kv/get "s2" "key1"))         =>       "v2")
+
+
+
+(facts "about KV protocols: if a key hasn't been set or remove should return `nil`"
+
+       ;; assertion                             expected result
+       (-> kvstore
+           (kv/set "s1" "key1" "v1")
+           (kv/set "s2" "key1" "v2")
+           (kv/get "s3" "key1"))         =>       nil
+
+       (-> kvstore
+           (kv/set "s1" "key1" "v1")
+           (kv/set "s2" "key1" "v2")
+           (kv/get "s1" "key5"))         =>       nil
+
+       (-> kvstore
+           (kv/set "s1" "key1" "v1")
+           (kv/set "s1" "key1" nil)
+           (kv/get "s1" "key1"))         =>       nil)
