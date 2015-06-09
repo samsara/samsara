65b478d96918cd9cc5f00a1bb4149e0dd1591c8b
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-06-09 22:51:21 +0100

    more tests

--- moebius/test/moebius/kv_test.clj
+++ moebius/test/moebius/kv_test.clj
@@ -4,60 +4,109 @@
   (:import [moebius.kv KV InMemoryKVstore]))
 
 
-(def kvstore (kv/make-in-memory-kvstore))
 
-
-
-(facts "about KV protocols: I should be able to set a property with any value"
+(facts "about KV protocol: I should be able to set a property with any value"
 
        ;; assertion                             expected result
-       (-> kvstore
+       (-> (kv/make-in-memory-kvstore)
            (kv/set "s1" "key1" "v1")
            (kv/get "s1" "key1"))         =>       "v1"
 
-       (-> kvstore
+       (-> (kv/make-in-memory-kvstore)
            (kv/set "s1" "key1" 1)
            (kv/get "s1" "key1"))         =>       1
 
-       (-> kvstore
+       (-> (kv/make-in-memory-kvstore)
            (kv/set "s1" "key1" true)
            (kv/get "s1" "key1"))         =>       true
 
-       (-> kvstore
+       (-> (kv/make-in-memory-kvstore)
            (kv/set "s1" "key1" :a-value)
            (kv/get "s1" "key1"))         =>       :a-value)
 
 
 
-(facts "about KV protocols: properties are independent across sourceId"
+(facts "about KV protocol: properties are independent across sourceId"
 
        ;; assertion                             expected result
-       (-> kvstore
+       (-> (kv/make-in-memory-kvstore)
            (kv/set "s1" "key1" "v1")
            (kv/set "s2" "key1" "v2")
            (kv/get "s1" "key1"))         =>       "v1"
 
-       (-> kvstore
+       (-> (kv/make-in-memory-kvstore)
            (kv/set "s1" "key1" "v1")
            (kv/set "s2" "key1" "v2")
            (kv/get "s2" "key1"))         =>       "v2")
 
 
 
-(facts "about KV protocols: if a key hasn't been set or remove should return `nil`"
+(facts "about KV protocol: if a key hasn't been set or remove should return `nil`"
 
        ;; assertion                             expected result
-       (-> kvstore
+       (-> (kv/make-in-memory-kvstore)
            (kv/set "s1" "key1" "v1")
            (kv/set "s2" "key1" "v2")
            (kv/get "s3" "key1"))         =>       nil
 
-       (-> kvstore
+       (-> (kv/make-in-memory-kvstore)
            (kv/set "s1" "key1" "v1")
            (kv/set "s2" "key1" "v2")
            (kv/get "s1" "key5"))         =>       nil
 
-       (-> kvstore
+       (-> (kv/make-in-memory-kvstore)
            (kv/set "s1" "key1" "v1")
            (kv/set "s1" "key1" nil)
            (kv/get "s1" "key1"))         =>       nil)
+
+
+
+
+
+(facts "about Tx-Log protocol: updates must be recorded in tx-log"
+
+       ;; assertion                             expected result
+       (-> (kv/make-in-memory-kvstore)
+           (kv/set "s1" "key1" "v1")
+           (kv/set "s1" "key1" "v2")
+           (kv/tx-log)
+           count)
+
+       =>       2
+
+
+
+       (-> (kv/make-in-memory-kvstore)
+           (kv/set "s1" "key1" "v1")
+           (kv/set "s1" "key1" "v2")
+           (kv/tx-log))
+
+       => [[1 ["s1" "key1"] "v1"]
+           [2 ["s1" "key1"] "v2"]]
+
+
+
+       (-> (kv/make-in-memory-kvstore)
+           (kv/set "s1" "key1" "v1")
+           (kv/set "s1" "key1" "v2")
+           (kv/set "s1" "key1" nil)
+           (kv/tx-log))
+
+       => [[1 ["s1" "key1"] "v1"]
+           [2 ["s1" "key1"] "v2"]
+           [3 ["s1" "key1"] nil]]
+
+
+
+       (let [kv0 (kv/make-in-memory-kvstore)
+             kv1 (-> kv0
+                     (kv/set "s1" "key1" "v1")
+                     (kv/set "s2" "key1" "vB")
+                     (kv/set "s1" "key1" "v2")
+                     (kv/set "s1" "key1" nil)
+                     (kv/set "s2" "key2" "vC"))
+
+             kv2 (-> (kv/make-in-memory-kvstore)
+                     (kv/restore (kv/tx-log kv1)))]
+
+         (kv/snapshot kv1) => (kv/snapshot kv2)))
