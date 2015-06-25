58a8106d9716913a3b136a76d0655f906af94e7e
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-06-25 13:40:42 +0100

    as demo of state management added a event-sequencer in the default pipeline

--- core/src/samsara_core/core.clj
+++ core/src/samsara_core/core.clj
@@ -1,5 +1,6 @@
 (ns samsara-core.core
   (:require [moebius.core :refer :all])
+  (:require [moebius.kv :as kv])
   (:require [digest :refer [sha-256]]))
 
 
@@ -60,12 +61,21 @@
       :source event})))
 
 
+(defenrich event-sequencer
+  #_"It adds a 'seqn' attribute to the event with a monotonic incremental counter
+    which helps to generate a sequence of the order the events have been processed."
+  [state {:keys [sourceId] :as event}]
+  (let [seqn   (inc (or (kv/get state sourceId :count) 0))
+        state' (kv/set state sourceId :count seqn)]
+    [state' (assoc event :seqn seqn)]))
+
 
 (defn make-samsara-pipeline [config]
   (pipeline
    inject-kibana-timestamp
    is-timestamp-reliable
    inject-id
+   event-sequencer
    (format-to-qanal (:index config))))
 
 
