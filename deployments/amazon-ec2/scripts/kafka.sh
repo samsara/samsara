3a82106e98ef64bd23d81661abb0270dbc807cc8
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-07-03 00:04:44 +0100

    continue work on aws template

--- deployments/amazon-ec2/scripts/kafka.sh
+++ deployments/amazon-ec2/scripts/kafka.sh
@@ -19,5 +19,5 @@ docker run -d --restart=on-failure:10 \
        -v /data/zk:/data \
        -e "KAFKA_BROKER_ID=1" \
        -e "ADV_IP=$(curl 'http://169.254.169.254/latest/meta-data/local-ipv4')" \
-       -e "ZOOKEEPER_PORT_2181_TCP_ADDR=$1"
+       -e "ZOOKEEPER_PORT_2181_TCP_ADDR=$1" \
        samsara/kafka
--- deployments/amazon-ec2/variables.tf
+++ deployments/amazon-ec2/variables.tf
@@ -3,6 +3,16 @@ variable "secret_key" {}
 variable "region" {
     default = "eu-west-1"
 }
+variable "zone1" {
+    default = "eu-west-1a"
+}
+variable "zone2" {
+    default = "eu-west-1b"
+}
+variable "zone3" {
+    default = "eu-west-1c"
+}
+
 variable "key_name" {
     default = ""
 }
