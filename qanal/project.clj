71f615ce0761cf37d99c05a0c563469f5a9baa3a
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-08-24 09:56:14 +0100

    bumping dependencies

--- qanal/project.clj
+++ qanal/project.clj
@@ -1,22 +1,22 @@
-(defproject qanal "0.2.1"
+(defproject qanal "0.3.0-SNAPSHOT"
   :description "An Application that bulk indexes docs from Kafka to Elasticsearch"
   :url "https://github.com/samsara/qanal"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.3.1"]            ;Command line parsing
-                 [com.taoensso/timbre "3.3.1"]              ;logging
-                 [clj-kafka "0.2.8-0.8.1.1"]                ;kafka
-                 [clojurewerkz/elastisch "2.1.0"]           ;elasticsearch
-                 [cheshire "5.4.0"]                         ;JSON
-                 [prismatic/schema "0.4.0"]                 ;validation
-                 [samsara/trackit "0.2.0"]                  ;Metrics tracking
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.cli "0.3.3"]
+                 [com.taoensso/timbre "3.4.0"]
+                 [clj-kafka "0.3.2"]
+                 [clojurewerkz/elastisch "2.1.0"]
+                 [cheshire "5.5.0"]
+                 [prismatic/schema "0.4.4"]
+                 [samsara/trackit "0.2.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  ]
   :main qanal.core
 
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[midje "1.6.3"]
+             :dev {:dependencies [[midje "1.7.0"]
                                   [midje-junit-formatter "0.1.0-SNAPSHOT"]]
                    :plugins [[lein-midje "3.1.3"]
                              [lein-bin "0.3.5"]]}}
