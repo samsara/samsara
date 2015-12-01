c9406d54b6d2e8025f1135b692c7b29a580ab009
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-12-01 13:52:36 +0000

    some ring-buffers tests without madness

--- clients/clojure/src/samsara/ring_buffer.clj
+++ clients/clojure/src/samsara/ring_buffer.clj
@@ -1,6 +1,5 @@
 (ns samsara.ring-buffer
-  (:require [amalloy.ring-buffer :as rb]
-            [taoensso.timbre :as log]))
+  (:require [amalloy.ring-buffer :as rb]))
 
 
 (defn- pop-while
@@ -21,38 +20,50 @@
 
 (defprotocol PRingBuffer
   "Protocol that the RingBuffer type must implement."
+
   (enqueue [this item]
-    "Add an item to the Ring Buffer.")
+           "Add an item to the Ring Buffer.")
+
   (dequeue [this items]
-    "Given a list of [id events] will remove the events which are still in the Ring Buffer.")
+           "Given a list of [id events] will remove the events which are still in the Ring Buffer.")
+
   (snapshot [this]
-    "Returns an immutable snapshot of the current state of the Ring Buffer.")
+            "Returns an immutable snapshot of the current state of the Ring Buffer.")
+
   (items [this]
-    "Returns a list containing just the items in the buffer.")
+         "Returns a list containing just the items in the buffer.")
+
   )
 
+
 (deftype RingBuffer [counter buffer]
-  ;;Ring Buffer Implementation for Samsara Client. This type uses amalloy/ring-buffer.
-  ;;Every item is assigned an unique id and added as a pair like [1 {:eventName...].
-  ;;This id will be used to remove the items which have been successfully processed.
-  ;;This type currently does not support (pop n) type operations. The snapshot function
-  ;;will return the current snapshot as-is. This wont help in scenarios where more than
-  ;;one consumer is involved.
+  ;; Ring Buffer Implementation for Samsara Client. This type uses
+  ;; amalloy/ring-buffer.  Every item is assigned an unique id and
+  ;; added as a pair like [1 {:eventName...}].  This id will be used to
+  ;; remove the items which have been successfully processed.  This
+  ;; type currently does not support (pop n) type operations. The
+  ;; snapshot function will return the current snapshot as-is. This
+  ;; wont help in scenarios where more than one consumer is involved.
 
   Object
   (toString [this]
     (pr-str buffer))
 
   PRingBuffer
+
   (enqueue [this item]
     (let [id (inc counter)]
       (RingBuffer. id (into buffer [[id item]]))))
+
   (snapshot [this] buffer)
+
   (items [this]
     (map second buffer))
+
   (dequeue [this items]
     (RingBuffer. counter (drop-items buffer items)))
   )
+
 
 (defn ring-buffer
   "Create an empty ring buffer with the specified [capacity]."
---
+++ clients/clojure/test/samsara/ring_buffer_test.clj
@@ -0,0 +1,117 @@
+(ns samsara.ring-buffer-test
+  (:require [samsara.ring-buffer :refer :all]
+            [midje.sweet :refer :all]))
+
+
+
+(fact "ring-buffer: create empty buffer has no items"
+
+      (-> (ring-buffer 3)
+          (items)) => '()
+
+      )
+
+
+(fact "ring-buffer: create and enqueue 1 item"
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (items)) => '(:a)
+
+      )
+
+
+(fact "ring-buffer: create and enqueue items to fill the buffer"
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (enqueue :b)
+          (enqueue :c)
+          (items)) => '(:a :b :c)
+
+      )
+
+
+(fact "ring-buffer: overrunning the buffer discards the oldest element"
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (enqueue :b)
+          (enqueue :c)
+          (enqueue :d)
+          (items)) => '(:b :c :d)
+
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (enqueue :b)
+          (enqueue :c)
+          (enqueue :d)
+          (enqueue :e)
+          (enqueue :f)
+          (items)) => '(:d :e :f)
+      )
+
+
+(fact "ring-buffer: create and enqueue 1 item and dequeue"
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (snapshot)) => [[1 :a]]
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (dequeue [[1 :a]])
+          (items)) => '()
+
+      )
+
+
+
+(fact "ring-buffer: dequeue removes only matching ids"
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (enqueue :a)
+          (snapshot)) => [[1 :a] [2 :a]]
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (enqueue :a)
+          (dequeue [[1 :a]])
+          (snapshot)) => [[2 :a]]
+
+      )
+
+
+(fact "ring-buffer: dequeue with no matching ids is a no-op"
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (enqueue :a)
+          (dequeue [[6 :a]])
+          (snapshot)) => [[1 :a] [2 :a]]
+
+      ;; no-op => before = after
+      (let [r  (-> (ring-buffer 3)
+                   (enqueue :a)
+                   (enqueue :a))
+            r1 (dequeue r [[6 :a]])]
+
+        (= r r1))
+
+      )
+
+
+(fact "ring-buffer: dequeue with old ids after overrun is a no-op"
+
+      (-> (ring-buffer 3)
+          (enqueue :a)
+          (enqueue :b)
+          (enqueue :c)
+          (enqueue :d)
+          (enqueue :e)
+          (enqueue :f)
+          (dequeue [[1 :a] [2 :a]])
+          (snapshot)) => [[4 :d] [5 :e] [6 :f]]
+      )
