aa2f0e149604672c3c2f2c99765d62722b8c3f53
DayoOliyide <dayo.oliyide@gmail.com>
2016-01-14 09:26:36 +0000

    Logger - test file not used

--- clients/logger/test/main/clojure/samsara/logger/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns samsara.logger.core-test
-  (:require [clojure.test :refer :all]
-            [samsara.logger.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
