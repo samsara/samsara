ad24f9acf5c64c5620b99fcb1f4b48843649e7cb
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-01-28 16:31:48 +0000

    add script to publish all snapshots

---
+++ bin/publish-snapshot-containers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+#
+# it pushes the snapshot containers to docker hub
+#
+
+
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+
+
+# push third-party containers
+docker push samsara/base-image-jdk7:snapshot
+docker push samsara/base-image-jdk8:snapshot
+docker push samsara/zookeeper:snapshot
+docker push samsara/kafka:snapshot
+docker push samsara/elasticsearch:snapshot
+docker push samsara/kibana:snapshot
+docker push samsara/monitoring:snapshot
+docker push samsara/spark-master:snapshot
+docker push samsara/spark-worker:snapshot
+
+# push internal containers
+docker push samsara/ingestion-api:snapshot
+docker push samsara/samsara-core:snapshot
+docker push samsara/qanal:snapshot
