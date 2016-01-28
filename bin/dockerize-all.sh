5437db2e274c3956bde3828bbf5ed65e1b7ba046
DayoOliyide <dayo.oliyide@gmail.com>
2016-01-28 15:52:32 +0000

    Fixed circle.yml, improved script

--- bin/dockerize-all.sh
+++ bin/dockerize-all.sh
@@ -7,24 +7,25 @@ export CUR=$(pwd)
 export BASE=`cd $(dirname $0)/.. && pwd`
 
 function dockerize(){
+    # $1 - dir
+    # $2 - repo_name
+    # $3 - tag
 
     echo '--------------------------------------------------------------------------------'
-    echo '              BUILIDING AND PUSHING DOCKER IMAGE FOR :' $*
+    echo '              BUILIDING AND PUSHING DOCKER IMAGE  :' $2:$3
     echo '--------------------------------------------------------------------------------'
 
-    cd $BASE/$*
+    cd $1
 
-    docker build -t samsara/$*:snapshot .
+    docker build -t $2:$3 .
     docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-    docker push samsara/$*:snapshot
+    docker push $2:$3
 
-    cd $CUR
+    cd - 
 }
 
 
-## build ingestion-api docker image
-dockerize ingestion-api
-
-
-## build qanal docker image
-dockerize qanal
+## build and push the internal containers
+dockerize  ingestion-api samsara/ingestion-api   snapshot
+dockerize  qanal         samsara/qanal           snapshot
+dockerize  core          samsara/samsara-core    snapshot
