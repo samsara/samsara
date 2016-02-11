90e0b6f926bb3efb6a1bd6df7149dc1747ad62c2
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-02-11 22:37:22 +0000

    updated synapse and more images cleanup

--- docker-images/els/configure-and-start.sh
+++ docker-images/els/configure-and-start.sh
@@ -6,8 +6,6 @@ export HOSTNAME=${HOSTNAME-els}
 export IP=${ADV_IP:-`ip ro get 8.8.8.8 |  grep src | sed -E "s/.* src (\S+)/\1/g"`}
 export ELS_HEAP_SIZE=${ELS_HEAP_SIZE:-`free -g | grep Mem: | awk '{printf( "%.0fg\n", $2 / 2 + 1 )}'`}
 
-export AWS_ACCESS_KEY=${AWS_ACCESS_KEY:-}
-export AWS_SECRET_KEY=${AWS_SECRET_KEY:-}
 export AWS_REPOS_ACCESS_KEY=${AWS_REPOS_ACCESS_KEY:-$AWS_ACCESS_KEY}
 export AWS_REPOS_SECRET_KEY=${AWS_REPOS_SECRET_KEY:-$AWS_SECRET_KEY}
 
