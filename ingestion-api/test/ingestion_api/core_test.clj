c5b98c5348e32f2e4929f892fd83e853df0161c7
Bruno Bonacci <bruno.bonacci@gmail.com>
2016-02-25 09:58:40 +0000

    removed unused test

--- ingestion-api/test/ingestion_api/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns ingestion-api.core-test
-  (:require [clojure.test :refer :all]
-            [ingestion-api.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
