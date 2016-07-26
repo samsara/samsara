6aa6667cce2bc05624d7e3daae34ed22a92adef2
Michael <michaelp193@gmail.com>
2016-07-26 14:24:36 -0400

    naive deque+threading implementation

--- clients/python/samsara_sdk/client.py
+++ clients/python/samsara_sdk/client.py
@@ -13,7 +13,25 @@ import gzip
 import json
 import time
 import requests
-from collections import deque, Iterable
+from collections import deque
+import logging
+from threading import Thread, Event
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def seconds_to_millis(s):
+    return int(s * 1000)
+
+
+def test_event():
+    return {
+        "timestamp": seconds_to_millis(time.time()),
+        "sourceId": "3aw4sedrtcyvgbuhjkn",
+        "eventName": "user.item.added",
+        "page": "orders",
+        "item": "sku-1234"
+    }
 
 
 class InvalidEvent(Exception):
@@ -31,11 +49,9 @@ class InvalidConfiguration(Exception):
 class COMPRESSION(object):
     GZIP = "gzip"
 
-    @staticmethod
     def compress_none(s):
         return json.dumps(s)
 
-    @staticmethod
     def compress_gzip(s):
         return gzip.compress(bytes(json.dumps(s), 'utf-8'))
 
@@ -44,7 +60,6 @@ class COMPRESSION(object):
         None: compress_none
     }
 
-    @staticmethod
     def get_compressor(compression):
         if compression not in COMPRESSION.COMPRESS.keys():
             raise InvalidArguement(
@@ -66,9 +81,9 @@ DEFAULT_CONFIG = {
     "start_publishing_thread": True,
 
     # how often should the events being sent to samsara
-    # in milliseconds
+    # in seconds
     # default 30s
-    "publish_interval": 30000,
+    "publish_interval": 30,
 
     # max size of the buffer, when buffer is full
     # older events are dropped.
@@ -103,40 +118,69 @@ event_validator = Validator(
 
 
 def validate_events(events):
-    if not isinstance(events, Iterable):
+    if not isinstance(events, list):
         events = [events]
     for e in events:
         if not event_validator.validate(e):
             raise InvalidEvent(
                 "Invalid event found, errors:\n{}"
                 .format(event_validator.errors))
+    return events
 
 
 def publish_events(url, events,
                    send_timeout=DEFAULT_CONFIG['send_timeout'],
                    compression=DEFAULT_CONFIG['compression']):
-    validate_events(events)
+    valid_events = validate_events(events)
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
         "Content-Encoding": compression or "identity",
-        PUBLISHED_TIMESTAMP: str(time.time())
+        PUBLISHED_TIMESTAMP: str(seconds_to_millis(time.time()))
     }
     compress = COMPRESSION.get_compressor(compression)
 
-    requests.post(
+    return requests.post(
         url + "/v1/events",
         headers=headers,
-        data=compress(events),
+        data=compress(valid_events),
         timeout=send_timeout
     )
+
+
+class IntervalTimer(Thread):
+    def __init__(self, interval, func):
+        Thread.__init__(self)
+        self.interval = interval
+        self.func = func
+        self.stopped = Event()
+
+    def run(self):
+        while not self.stopped.wait(self.interval):
+            self.func()
+
+    def stop(self):
+        self.stopped.set()
 
 
 class SamasaraClient(object):
     def __init__(self, **config):
         # init client with config
-        config = dict(DEFAULT_CONFIG, **config)
+        self._init_config(dict(DEFAULT_CONFIG, **config))
+        if self.config['start_publishing_thread']:
+            self.start_consuming()
 
+    def start_consuming(self):
+        self.timer = self.get_timer(self.config['publish_interval'])
+        logging.debug(
+            'Starting publishing thread at an {}sec interval'
+            .format(self.config['publish_interval']))
+        self.timer.start()
+
+    def stop_consuming(self):
+        self.timer.stop()
+
+    def _init_config(self, config):
         if not config['url']:
             raise InvalidConfiguration(
                 "Missing Samsara's ingestion api endpoint url.")
@@ -144,24 +188,45 @@ class SamasaraClient(object):
             raise InvalidConfiguration(
                 "Publish interval needs to be above 0, not {}"
                 .format(config['publish_interval']))
-        if config['min-buffer-size'] > config['max-buffer-size'] \
-           or config['min-buffer-size'] == 0:
-            config['min-buffer-size'] = 1
+        if config['min_buffer_size'] > config['max_buffer_size'] \
+           or config['min_buffer_size'] == 0:
+            config['min_buffer_size'] = 1
 
-        self.buffer = deque(maxlen=config['max-buffer-size'])
+        self.buffer = deque(maxlen=config['max_buffer_size'])
 
         self.config = config
 
+    def _flush_buffer(self):
+        self.publish_events(
+            [self.buffer.popleft() for _ in range(len(self.buffer))]
+        )
+
+    def _flush_buffer_if_ready(self):
+        if len(self.buffer) >= self.config['min_buffer_size']:
+            self._flush_buffer()
+
+    def get_timer(self, interval):
+        return IntervalTimer(interval, self._flush_buffer_if_ready)
+
     def record_event(self, event):
-        # add to buffer, if buffer is at capacity, flush then add
         if not event.get('sourceId'):
             event['sourceId'] = self.config['sourceId']
         if not event.get('timestamp'):
-            event['timestamp'] = time.time()
+            event['timestamp'] = seconds_to_millis(time.time())
+
         validate_events(event)
-        self.deque.append(event)
+        self.buffer.append(event)
 
     def publish_events(self, events):
-        publish_events(self.config['url'], events,
-                       send_timeout=self.config['send_timeout'],
-                       compression=self.config['compression'])
+        count = len(events)
+        res = publish_events(self.config['url'], events,
+                             send_timeout=self.config['send_timeout'],
+                             compression=self.config['compression'])
+        if res.ok:
+            logging.debug(
+                'Successfully submitted {} events'
+                .format(count))
+        else:
+            logging.error(
+                '{} events were not successfully submitted'
+                .format(count))
