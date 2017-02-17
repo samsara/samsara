444dd70295fb575144eabdeda2a5c4a78ad5a70c
kolotaev <kolotaev@tut.by>
2017-02-17 15:38:02 +0300

    Add EDN Scribe serializer

---
+++ internal/scribe/test/samsara/scribe/edn_test.clj
@@ -0,0 +1,42 @@
+(ns samsara.scribe.edn-test
+  (:require  [midje.sweet :refer :all]
+             [samsara.scribe.serializer.edn :as edn]
+             [samsara.scribe.protocol :as scribe]))
+
+
+(facts "edn serializer"
+
+  (fact "roundtrip edn"
+
+    (let [s (edn/make-edn-scribe)
+          data {:test 1
+                :foo "bar"
+                :valid? true
+                :date "2017-02-04T19:41:53.206Z"
+                :value 2.3}]
+
+      (->> (scribe/write s data)
+        (scribe/read s))
+      => data))
+
+  (fact "roundtrip nil"
+
+    (let [s (edn/make-edn-scribe)
+          data nil]
+
+      (->> (scribe/write s data)
+        (scribe/read s))
+      => data))
+
+  (fact "roundtrip record"
+
+    (defrecord Event [number owner])
+
+    (let [event-reader {'samsara.scribe.edn_test.Event map->Event}
+          s (edn/make-edn-scribe {:readers event-reader})
+          data (->Event 123, "John Doe")]
+
+      (->>
+        (scribe/write s data)
+        (scribe/read s))
+      => data)))
