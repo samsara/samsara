25f1487f1984e4806c4acd2d107669c4ff32fdf7
Bruno Bonacci <bruno.bonacci@gmail.com>
2017-04-16 00:22:41 +0100

    state-machines: start cleaning [CI SKIP]

--- internal/machina/project.clj
+++ internal/machina/project.clj
@@ -12,8 +12,11 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.brunobonacci/safely "0.2.4"]]
+                 [com.brunobonacci/safely "0.2.4"]
+                 [org.clojure/tools.logging "0.3.1"]]
 
-  :profiles {:dev {:dependencies [[midje "1.9.0-alpha6"]
-                                  [criterium "0.4.4"]]
+  :profiles {:dev {:resource-paths ["dev-resources"]
+                   :dependencies [[midje "1.9.0-alpha6"]
+                                  [criterium "0.4.4"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.25"]]
                    :plugins [[lein-midje "3.2.1"]]}})
--- internal/machina/src/samsara/machina/core.clj
+++ internal/machina/src/samsara/machina/core.clj
@@ -1,5 +1,7 @@
 (ns samsara.machina.core
-  (:require [safely.core :as safely]))
+  (:require [clojure.tools.logging :as log]
+            [safely.core :as safely]))
+
 ;;
 ;; This namespace contains an REPL exploration session on
 ;; various possible implementation of a State Machine
@@ -22,386 +24,29 @@
 ;;
 
 
-
-(defn write-to-file [f]
-  (spit f
-        (str (java.util.Date.) \newline)
-        :append true))
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                                                            ;;
-;;                  ---==| F I R S T   A T T E M P T |==----                  ;;
-;;                                                                            ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(comment
-
-  (def sm
-    {:state       :machina/start
-     :data        nil
-     :transitions
-     {:machina/start {:fn*        (fnil inc 0)
-                      :on-success :machina/start
-                      :on-failure :machina/stop}}}
-    )
-
-  ;;
-  ;; TODO: interesting approach but there is no way
-  ;; for the `fn*` to know when to stop
-  ;; or to `on-success` to decide which new
-  ;; state to go. For example if the counter
-  ;; gets to 10 I would like to exit
-  ;; Same for the `on-failure` it should be
-  ;; able to decide which state based on error
-  ;; TODO: `on-success` and `on-failure` should
-  ;; take the state in input `(on-success sm)`
-  ;; or `(on-failure sm ex)`.
-  ;;
-  (defn play [{:keys [state transitions data] :as sm}]
-    (if (= state :machina/stop)
-      sm
-      (if-let [{:keys [fn* on-success on-failure]} (get transitions state)]
-        (try
-          (let [data1 (apply fn* [data])]
-            (assoc sm :data data1 :state on-success))
-          (catch Exception x
-            (assoc sm
-                   :error {:from-state state
-                           :error x}
-                   :state on-failure)))
-        (assoc sm :error {:from-state state
-                          :error :machina/bad-state}))))
+(defn log-state-change
+  "This wrapper logs the state machine transitions to the configured
+  logger. The default log level is :debug, but you can specify a
+  different level."
+  ([handler]
+   (log-state-change :debug handler))
+  ([level handler]
+   (fn [sm1]
+     (let [sm2 (handler sm1)]
+       (log/logp (or level :debug) "transition:" (:state sm1) "->" (:state sm2))
+       sm2))))
 
 
-  (comment
-    (iterate play sm)
-    ))
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                                                            ;;
-;;                 ---==| S E C O N D   A T T E M P T |==----                 ;;
-;;                                                                            ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn epoch-counter
+  "This wrapper increments a counter on every transition.
+  Useful to determine whether the state machine is progressing."
+  [handler]
+  (fn [sm]
+    (handler (update sm :epoch (fnil inc 0)))))
 
 
 
 (comment
-
-
-
-  ;; counter example
-  (def sm
-    {:state :machina/start
-     :data   nil
-     :transitions
-     {:machina/start {:fn*        (fnil inc 0)
-                      :on-success (fn [{:keys [data]}]
-                                    (if (= data 10)
-                                      :machina/stop
-                                      :machina/start))
-                      :on-failure (constantly :machina/stop)} }})
-
-  (defn do! [f]
-    (fn [v]
-      (f v)
-      v))
-
-
-  (defn transition [{:keys [state transitions data] :as sm}]
-    (if (= state :machina/stop)
-      sm
-      (if-let [{:keys [fn* on-success on-failure]} (get transitions state)]
-        (try
-          (as-> sm $
-            (assoc $ :data  (apply fn* [data]))
-            (assoc $ :state (on-success $)))
-          (catch Exception x
-            (as-> sm $
-              (assoc $ :state (on-failure $))
-              (dissoc $ :error))))
-        (assoc sm :error {:from-state state
-                          :error :machina/bad-state}))))
-
-
-
-
-  (comment
-    (transition sm)
-    (->> sm
-         (iterate transition)
-         (take-while #(not= :machina/stop (:state %)))
-         #_(map :data)
-         last
-         )
-    )
-
-
-
-
-  ;; write to file example
-  (def sm
-    {:state :machina/start
-     :data   nil
-     :transitions
-     {:machina/start {:fn*        (constantly "/tmp/1/2/3/4/5/file.txt")
-                      :on-success (constantly :write-to-file)
-                      :on-failure (constantly :machina/stop)}
-
-
-      :sleep {:fn*        (do! (fn [_] (Thread/sleep 1000)))
-              :on-success (constantly :write-to-file)
-              :on-failure (constantly :sleep)}
-
-
-      :write-to-file {:fn*        (do! write-to-file)
-
-                      :on-success (fn [sm] (println "OK")   (clojure.pprint/pprint sm) :sleep)
-                      :on-failure (fn [sm] (println "FAIL") (clojure.pprint/pprint sm) :sleep)}}})
-
-
-  (comment
-
-    (-> sm
-        transition
-        transition
-        )
-
-    (->> sm
-         (iterate transition)
-         (take-while #(not= :machina/stop (:state %)))
-         (last))
-
-
-
-    )
-
-
-  )
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                                                            ;;
-;;                  ---==| T H I R D   A T T E M P T |==----                  ;;
-;;                                                                            ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(comment
-
-  ;; write to file example
-
-  ;;
-  ;; Most generic approach
-  ;; but not much re-use.
-  ;; also hard to make cross-cutting concerns
-  ;; like a function which displays all
-  ;; the state transitions or even
-  ;; measure the time spent in each state.
-  ;;
-  (defmulti transition :state)
-
-  (defmethod transition :machina/stop
-    [sm]
-    sm)
-
-
-  (defmethod transition :machina/start
-    [sm]
-    (assoc sm
-           :data "/tmp/1/2/3/4/5/file.txt"
-           :state :write-to-file))
-
-
-  (defmethod transition :write-to-file
-    [{f :data :as sm}]
-    (try
-      (write-to-file f)
-      (assoc sm :state :sleep)
-      (catch Exception x
-        (assoc sm :state :sleep))))
-
-
-  (defmethod transition :sleep
-    [sm]
-    (Thread/sleep 1000)
-    (assoc sm :state :write-to-file))
-
-  (comment
-
-    (-> sm
-        transition
-        transition
-        transition
-        )
-
-    (->> sm
-         (iterate transition)
-         (take-while #(not= :machina/stop (:state %)))
-         (last))
-
-
-
-    ))
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                                                            ;;
-;;                 ---==| F O U R T H   A T T E M P T |==----                 ;;
-;;                                                                            ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;
-;; this time i'm going to use the same basic and generic idea of
-;; a f(stm) -> stm' but without the multi-method dispatch
-;; so that it is possible to add wrappers/filters style functions
-;; which handle cross/cutting concerns.
-;;
-
-(comment
-
-  (defn log-state-change [handler]
-    (fn [sm1]
-      (let [sm2 (handler sm1)]
-        (println "transition: " (:state sm1) "->" (:state sm2))
-        sm2)))
-
-  (defn epoch-counter
-    [handler]
-    (fn [sm]
-      (handler (update sm :epoch (fnil inc 0)))))
-
-  (defn- wrapper
-    ([] identity)
-    ([f] f)
-    ([f g] (g f))
-    ([f g & fs]
-     (reduce wrapper (concat [f g] fs))))
-
-  (comment
-    (defn mh [n]
-      (fn [h]
-        (fn [sm]
-          (println n)
-          (h sm))))
-
-    (def f1 (mh 1))
-    (def f2 (mh 2))
-    (def f3 (mh 3))
-    (def f4 (mh 4))
-
-    ((wrapper identity f4 f3 f2 f1) {}))
-
-
-  ;; write to file example
-  (def sm
-    {:state :machina/start
-     :epoch 0
-     :data nil
-
-     :dispatch
-     {:machina/stop identity
-      :machina/start
-      (fn
-        [sm]
-        (assoc sm
-               :data "/tmp/1/2/3/4/5/file.txt"
-               :state :write-to-file))
-
-      :write-to-file
-      (fn
-        [{f :data :as sm}]
-        (try
-          (write-to-file f)
-          (assoc sm :state :sleep)
-          (catch Exception x
-            (assoc sm :state :sleep))))
-
-      :sleep
-      (fn
-        [sm]
-        (Thread/sleep 1000)
-        (assoc sm :state :write-to-file))}
-
-     :wrappers
-     [#'epoch-counter #'epoch-counter #'log-state-change]})
-
-
-  (defn bad-state [sm]
-    (throw (ex-info "Invalid state" sm)))
-
-  ;; this seems to maintain the simplicity of
-  ;; the earlier attempts with the general
-  ;; approach of the multi-method
-  ;; without scarifying the ability to
-  ;; manage cross-cutting concerns.
-  ;; note: are wrappers the best approach?
-  ;; maybe something like pedestal stacklets
-  ;; would be easier?
-
-  (defn transition
-    [{:keys [state data dispatch wrappers] :as sm}]
-    (let [f0 (get dispatch state bad-state)
-          f  (apply wrapper (cons f0 (reverse wrappers)))]
-      (f sm)))
-
-
-  (comment
-
-    (-> sm
-        transition
-        transition
-        ;;transition
-        )
-
-    (->> sm
-         (iterate transition)
-         (take-while #(not= :machina/stop (:state %)))
-         (last))
-
-
-    ))
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                                                            ;;
-;;       ---==| E X P A N D I N G   F O U R T H   A T T E M P T |==----       ;;
-;;                                                                            ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;
-;; same as before just now expanding the capabilities
-;; of generic wrappers and common pattern/policies for
-;; error handling (WORK IN PROGRESS)
-;;
-
-(comment
-
-  (defn log-state-change [handler]
-    (fn [sm1]
-      (let [sm2 (handler sm1)]
-        (println "transition: " (:state sm1) "->" (:state sm2))
-        sm2)))
-
-  (defn epoch-counter
-    [handler]
-    (fn [sm]
-      (handler (update sm :epoch (fnil inc 0)))))
-
-  ;; not sure about this
-
-  (comment
-    ((error-state
-      (fn [_] (throw (ex-info "boom" {}))))
-     {:state :foo}))
 
   (defn error-state
     [handler]
@@ -560,3 +205,13 @@
 
 
     ))
+
+
+(comment
+
+  (defn write-to-file [f]
+    (spit f
+          (str (java.util.Date.) \newline)
+          :append true))
+
+  )
